167fd5fa8878c36593150b633bd66a9daa6e3097
jdanielOCUL <danielj@utl-192-194.library.utoronto.ca>
2014-09-17 10:39:44 -0400

    Initial Commit

---
+++ GeoMap/GeoMapDocument.h
@@ -0,0 +1,13 @@
+//
+//  GeoMapDocument.h
+//  GeoMap
+//
+//  Created by John Daniel on 2014-09-17.
+//  Copyright (c) 2014 John Daniel. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface GeoMapDocument : NSDocument
+
+@end
---
+++ GeoMap/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  GeoMap
+//
+//  Created by John Daniel on 2014-09-17.
+//  Copyright (c) 2014 John Daniel. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+  return NSApplicationMain(argc, argv);
+}
